c022222f06da80b0662d05a313bb137e0a44cc15
matheus fabiano barbosa aguilar <132641416+matheusarsenal@users.noreply.github.com>
2025-12-11 18:16:29 -0300

    Update listen vettel.py

--- listen vettel.py
+++ listen vettel.py
@@ -1,5 +1,6 @@
 import fastf1
-session = fastf1.get_session(2019, 'Monza', 'Q')
+session = fastf1.get_session(2019, 'Gilles Villanueve', 'R')
 session.load(telemetry=False, laps=False, weather=False)
 vettel = session.get_driver('VET')
+
 print(f"Pronto {vettel['FirstName']}?")
